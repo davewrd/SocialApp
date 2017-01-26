f8b1534f35bb7e240138e2c75d491c89ed516037
David Ward <davewrd00@gmail.com>
2017-01-26 00:07:25 +0000

    Sorted out all of facebook and firebase intergration

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInVC.swift
 //  SocialApp
 //
 //  Created by David on 25/01/2017.
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,31 @@ class SignInVC: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func facebookBtnTapped(_ sender: Any) {
+    
+    let facebookLogin = FBSDKLoginManager()
+    facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+      if error != nil {
+        print("DAVID: Unable to authenticate with facebook - \(error)")
+      } else if result?.isCancelled == true {
+        print("DAVID: User cancelled facebook authentication'")
+      } else {
+        print("DAVID: Successfully authenticated with Facebook")
+        let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+        self.firebaseAuth(credential)
+      }
+    }
+  }
+  
+  func firebaseAuth(_ credential: FIRAuthCredential) {
+    FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+      if error != nil {
+        print("DAVID: Unable to authenticate with Firebase - \(error)")
+      } else {
+        print("DAVID: Successfully been able to authenticate with Firebase")
+      }
+    })
+  }
 
 }
 
